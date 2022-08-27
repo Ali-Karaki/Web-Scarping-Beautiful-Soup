42bf2fbf46d76bf680bdec576d7282ab8f3dd7c5
Ali Karaki <ali.m.karaki@gmail.com>
2022-08-27 13:45:28 +0300

    get product directory section

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+
+siteUrl = 'https://docs.microsoft.com/en-us/'
+azureDocs = requests.get(siteUrl).text
+soup = BeautifulSoup(azureDocs, 'lxml')
+
+productDirectory_Section = soup.find('ul', class_="has-three-text-columns").find_all('a')
+productDirectory_Scraped = [product.text.strip() for product in productDirectory_Section]
+# print(productDirectory_Scraped)
+
